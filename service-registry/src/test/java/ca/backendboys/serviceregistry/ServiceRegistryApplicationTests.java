27526fa10882d18b97d983d139bf2d9c48eb5faa
Vikramjeet Singh <70337807+vsingh2201@users.noreply.github.com>
2025-11-23 16:33:11 -0500

    Add Service Registry and Spring Cloud API Gateway

---
+++ service-registry/src/test/java/ca/backendboys/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.backendboys.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
